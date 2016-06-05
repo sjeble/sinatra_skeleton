c9747022024151820b0fae4a5030bc99a511e5d2
Sean Eble <email": null>
2016-06-05 12:48:29 -0500

    added user controller, views, view helper

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
+require 'bcrypt'
 
-
-
+class User < ActiveRecord::Base
   def password
     @password ||= BCrypt::Password.new(hashed_password)
   end
@@ -10,7 +10,7 @@
     self.hashed_password = @password
   end
 
-
   def authenticate?(email, password)
     self.email == email && self.password == password
   end
+end
